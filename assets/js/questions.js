a2245627eeca3e704f80d4f575297984e856318b
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-01-04 14:45:23 -0500

    Finished questions, tweaked some styling

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,15 +2,15 @@
 var questions = [
   {
     title:
-    '1',
-    choices: ['', '', '', ''],
-    answer: '',
+    'Which of these are the largest HTML heading?',
+    choices: ['Head', 'H1', 'H5', 'Heading'],
+    answer: 'H1',
   },
   {
     title:
-    '2',
-    choices: ['', '', '', ''],
-    answer: '',
+    'Who is making the Web standards?',
+    choices: ['Google', 'Mozilla', 'The World Wide Web Consortium', 'United Nations'],
+    answer: 'The World Wide Web Consortium',
   },
   {
     title: 'Arrays in JavaScript can be used to store ____.',
@@ -24,9 +24,9 @@ var questions = [
   },
   {
     title:
-    '4',
-    choices: ['', '', '', ''],
-    answer: '',
+      'Where do you link style sheets?',
+    choices: ['First <p></p> tag', 'HTML Footer', 'Directory', 'HTML Header'],
+    answer: 'HTML Header',
   },
   {
     title: 'The condition in an if / else statement is enclosed within ____.',
@@ -35,21 +35,15 @@ var questions = [
   },
   {
     title:
-    '6',
-    choices: ['', '', '', ''],
-    answer: '',
+      'Styling can be inserted into which of the following?',
+    choices: ['.HTML', '.JS', '.CSS', 'All of the above'],
+    answer: 'All of the above',
   },
   {
     title:
     'String values must be enclosed within ____ when being assigned to variables.',
     choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
     answer: 'quotes',
-  },
-  {
-    title:
-    '8',
-    choices: ['', '', '', ''],
-    answer: '',
   },
   {
     title: 'Commonly used data types DO NOT include:',
@@ -61,6 +55,12 @@ var questions = [
     'A very useful tool used during development and debugging for printing content to the debugger is:',
     choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
     answer: 'console.log',
+  },
+  {
+    title:
+    'Extra: What is the first song ever sung by a computer?',
+    choices: ['Twinkle Twinkle Little Star', 'Daisy Bell', 'Mr. President', 'Jingle Bells'],
+    answer: 'Daisy Bell',
   },
 ];
 
